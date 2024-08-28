00f3b3ef7d890528286e71f0edad28740228f1bf
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2024-08-28 10:56:07 -0700

    mongoose

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/dbname', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const express = require('express'),
     app = express(),
