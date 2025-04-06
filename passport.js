abb2c5083869a7485bed8aaea578b13793918b67
LoganMartin93 <166079175+LoganMartin93@users.noreply.github.com>
2025-04-05 21:19:27 -0700

    Add JSDoc setup and update core files with documentation

--- passport.js
+++ passport.js
@@ -7,49 +7,69 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'Username',
-        passwordField: 'Password',
-      },
-      async (username, password, callback) => {
-        console.log(`${username} ${password}`);
-        await Users.findOne({ Username: username })
+/**
+ * Passport Local Strategy for authenticating users via username and password.
+ * @param {string} username - The username provided by the user.
+ * @param {string} password - The password provided by the user.
+ * @param {Function} callback - The callback function to return the result of the authentication.
+ * @returns {void}
+ */
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',  // Field used for username
+      passwordField: 'Password',  // Field used for password
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      // Find the user in the database by username
+      await Users.findOne({ Username: username })
         .then((user) => {
           if (!user) {
             console.log('incorrect username');
+            // If user is not found, return an error message
             return callback(null, false, {
               message: 'Incorrect username or password.',
             });
           }
           if (!user.validatePassword(password)) {
             console.log('incorrect password');
+            // If password validation fails, return an error message
             return callback(null, false, { message: 'Incorrect password.' });
           }
           console.log('finished');
+          // If authentication is successful, return the user object
           return callback(null, user);
         })
         .catch((error) => {
           if (error) {
             console.log(error);
+            // If an error occurs during the process, return the error
             return callback(error);
           }
-        })
-      }
-    )
-  );
+        });
+    }
+  )
+);
 
-
+/**
+ * Passport JWT Strategy for authenticating users based on JWT.
+ * @param {Object} jwtPayload - The payload decoded from the JWT.
+ * @param {Function} callback - The callback function to return the result of the JWT authentication.
+ * @returns {void}
+ */
 passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),  // Extract JWT from authorization header as bearer token
+  secretOrKey: 'your_jwt_secret'  // Secret key used to verify the JWT
 }, async (jwtPayload, callback) => {
+  // Find the user by the decoded user ID from the JWT
   return await Users.findById(jwtPayload._id)
     .then((user) => {
+      // If user is found, return the user object
       return callback(null, user);
     })
     .catch((error) => {
-      return callback(error)
+      // If an error occurs, return the error
+      return callback(error);
     });
 }));
--- server.js
+++ server.js
@@ -2,35 +2,60 @@ const http = require('http'),
   fs = require('fs'),
   url = require('url');
 
+/**
+ * Creates an HTTP server that serves static HTML pages and logs requests to a file.
+ * The server handles requests by serving either the `index.html` or `documentation.html` 
+ * file based on the URL path.
+ *
+ * @function
+ * @param {Object} request - The HTTP request object.
+ * @param {Object} response - The HTTP response object.
+ * @returns {void}
+ */
 http.createServer((request, response) => {
-  let addr = request.url,
-    q = new URL(addr, 'http://' + request.headers.host),
-    filePath = '';
+  let addr = request.url,  // The requested URL.
+    q = new URL(addr, 'http://' + request.headers.host),  // Parsed URL object.
+    filePath = '';  // Default file path.
 
+  /**
+   * Logs the URL request and timestamp to a log file.
+   * @function
+   * @param {Error|null} err - The error object if an error occurred, or null.
+   * @returns {void}
+   */
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
     if (err) {
-      console.log(err);
+      console.log(err);  // Logs any error encountered during the logging process.
     } else {
-      console.log('Added to log.');
+      console.log('Added to log.');  // Logs success when the entry is added to the log file.
     }
   });
 
+  // Checks the URL path and determines which HTML file to serve.
   if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
+    filePath = (__dirname + '/documentation.html');  // Serve documentation page.
   } else {
-    filePath = 'index.html';
+    filePath = 'index.html';  // Serve the default index page.
   }
 
+  /**
+   * Reads the chosen HTML file and serves it as the response.
+   * @function
+   * @param {Error|null} err - The error object if an error occurred, or null.
+   * @param {Buffer|string} data - The content of the file to serve.
+   * @returns {void}
+   */
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      throw err;
+      throw err;  // If there's an error reading the file, throw it.
     }
 
+    // Respond with the correct HTML content.
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
-
+    response.write(data);  // Writes the content to the response.
+    response.end();  // Ends the response.
   });
 
-}).listen(8080);
+}).listen(8080);  // Listens on port 8080 for incoming requests.
+
 console.log('My test server is running on Port 8080.');
